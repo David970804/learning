67dc2917f0098e62b411cf5f633fbc221d30ea85
David970804 <david19970804@gmail.com>
2018-07-17 09:27:20 +0800

    prepare to connect to remote repository

---
+++ selflearn1.py
@@ -0,0 +1,65 @@
+"""
+trial 1
+multiple = "1 + "+\
+		   "2 = "+\
+		   "3"	
+print(multiple)		  
+"""
+###########################################################
+"""
+trial 2 
+a,b,c=1,"2",'this is a testing string'
+print(a,b,c)
+"""
+###########################################################
+
+#print(string[-1:])
+"""
+trial3
+a = 11.21
+print(isinstance(a,float))
+del a
+#print(a)  #error message: a is not defined
+#print(21/4) # / return a float number
+#print(21//4) # // return a integer
+#print(r"testing\n")
+#new concept: tuples
+tuples=('123','kar98',123)
+print(tuples) #elements in a tuple cannot be replaced
+a="abcde"
+a[1]='h' #string is like tuple, its characters cannot be replaced as well
+print(a)
+"""
+
+###########################################################
+
+"""
+trial4
+tup=(20,) #a comma is needed when defining a one element tuple
+#print(tup)
+#new concept: set {} (different from Dictionary)
+
+st={'abcde'}  #'abcde'
+st=set('abcde') #{'d', 'e', 'c', 'a', 'b'}
+print(st) #自动去掉set中重复的元素
+
+a = set('abracadabra')
+b = set('alacazam')
+ 
+print(a)
+print(a - b)     # a和b的差集
+print(a | b)     # a和b的并集
+print(a & b)     # a和b的交集
+print(a ^ b)     # a和b中不同时存在的元素
+
+#创建一个空集合必须用 set() 而不是 { }，因为 { } 是用来创建一个 empty Dictionary。
+"""
+###########################################################
+
+
+
+
+
+
+
+
