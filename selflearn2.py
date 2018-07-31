2b2f00c05d5a9af70df7eae8eb1b4866a476cebc
David970804 <david19970804@gmail.com>
2018-07-31 16:57:58 +0800

    selflearn2 trial1 completed

--- selflearn2.py
+++ selflearn2.py
@@ -1,1 +1,16 @@
-#Trial 1
+"""
+#Trial 1 some operators
+a =20; c=2
+b=6
+c **=b
+print(a/b) #除
+print(a//b) #整除
+print(a**b) #乘幂
+print(a%b) #除余
+print(c) #c=c**b
+"""
+
+#####################################
+
+#Trial 2 binary operations
+
