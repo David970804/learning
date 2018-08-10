1efbd3813ac43721dcaa81f72bbc455e73e283a9
David970804 <david19970804@gmail.com>
2018-08-10 14:56:47 +0800

    numbers completed learning

--- selflearn3.py
+++ selflearn3.py
@@ -1,1 +1,38 @@
-#Trial 1 delete variables
+from math import *
+import random
+"""
+#Trial 1 delete variables and number types
+var1=1
+print(var1)
+del var1
+#print(var1) #var1 is not defined
+
+#complex
+var = 45j+3
+var1=complex(3,45)
+print(var)
+print(var1)
+"""
+
+########################################################
+
+"""
+#Trial 2 numbers
+#some functions
+x=20
+print(complex(x)) # 将x变成复数，实部为x虚部为0
+print(abs(-10))  #绝对值
+#import math
+print(ceil(4.1)) #向上取整
+print(exp(1))  #exponential
+print(floor(4.5)) #向下取整
+print(round(4.325,2)) #四舍五入
+print(cos(pi))  #三角函数 
+print(acos(-1)) #反三角函数
+#random numbers, import random
+print(random.random()) #[0,1)内随机数
+print(random.uniform(0,1)) #[x,y]内随机数
+"""
+########################################################
+
+#Trial 3 Strings
